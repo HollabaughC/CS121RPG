257464480bfa13bba9e237c5ba33d286574004a7
Kevin <barbierikevinm@gmail.com>
2024-11-05 13:41:44 -0500

    Questions read from .json now

--- CS121RPG/Assets/Scripts/MCQuestions.cs
+++
@@ -1,8 +0,0 @@
-[System.Serializable]
-public class MCQuestions
-{
-    public string question;
-    public string[] answers;
-    public int correctAnswer;
-
-}
--- CS121RPG/Assets/Scripts/QuizManagerMC.cs
+++ CS121RPG/Assets/Scripts/QuizManagerMC.cs
@@ -4,32 +4,46 @@ using System.Collections.Generic;
 
 public class QuizManagerMC : MonoBehaviour
 {
-    public List<MCQuestions> qList;
+    public JSONQuestionParser JQP;
     public GameObject[] options = new GameObject[4];
+    public List<int> qIndexOptions;
     public int qIndex;
+    public int uIndex = 0;
     public Text qText;
     
     void Start(){
-        generateQuestions();
+        generateQuestionList();
     }
 
     public void correct(){
-        qList.RemoveAt(qIndex);
-        generateQuestions();
+        qIndexOptions.RemoveAt(qIndex);
+        if(qIndexOptions.Count > 0)
+            generateQuestions();
+        else
+            Debug.Log("You Finished!");
     }
-    void setAnswer() {
+    
+    void setAnswers() {
         for(int i = 0; i < options.Length; i++){
             options[i].GetComponent<MCAnswers>().isCorrect = false;
-            options[i].transform.GetChild(0).GetComponent<Text>().text = qList[qIndex].answers[i];
-            if(i == qList[qIndex].correctAnswer){
+            options[i].transform.GetChild(0).GetComponent<Text>().text = JQP.data.unit[uIndex].question[qIndexOptions[qIndex]].answers[i];
+            Debug.Log("setting answer to " + JQP.data.unit[uIndex].question[qIndexOptions[qIndex]].answers[i]);
+            if(i == JQP.data.unit[uIndex].question[qIndexOptions[qIndex]].correct){
                 options[i].GetComponent<MCAnswers>().isCorrect = true;
             }
         }
     }
 
     void generateQuestions() {
-        qIndex = Random.Range(0, qList.Count);
-        qText.text = qList[qIndex].question;
-        setAnswer();
+        qIndex = Random.Range(0, qIndexOptions.Count);
+        qText.text = JQP.data.unit[uIndex].question[qIndexOptions[qIndex]].text;
+        setAnswers();
+    }
+    void generateQuestionList() {
+        qIndexOptions = new List<int>();
+        for(int i = 0; i < JQP.data.unit[uIndex].question.Count; i++){
+            qIndexOptions.Add(i);
+        }
+        generateQuestions();        
     }
 }
