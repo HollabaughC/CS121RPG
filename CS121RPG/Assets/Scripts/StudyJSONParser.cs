b8dd7a80dc9357a28850e3f0480392cf6e9e5c4e
Kevin <barbierikevinm@gmail.com>
2024-11-30 23:03:51 -0500

    for camerongit add .

--- CS121RPG/Assets/Scripts/StudyJSONParser.cs
+++ CS121RPG/Assets/Scripts/StudyJSONParser.cs
@@ -6,10 +6,32 @@ public class StudyJSONParser : MonoBehaviour
 {
     public JsonDataStudy data;
     string jsonPath = "Assets/Scripts/studyjson.json";
+    bool userIn = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Start() {
         string jsonString = File.ReadAllText(jsonPath);
         data = JsonUtility.FromJson<JsonDataStudy>(jsonString); //jsonUtility.FromJson() requires attributes to be case-sensitive.
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("user entered!");
+        userIn = true;
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        // Check if the player exits the trigger area
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("player left :(");
+            userIn = false;
+        }
+    }
+    void Update() {
+        if(PlayerPrefs.GetInt("DayCount") % 4 != 0){
+            if(userIn && Input.GetKeyDown(KeyCode.E)){
+                Debug.Log("#Studyinnnn");
+            }
+        }
     }
     
 }
